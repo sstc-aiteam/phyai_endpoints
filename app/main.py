9af2ef391abbca32cca7bff91a947b809a30fb27
Eric0329 <ericyang0329@gmail.com>
2026-03-17 11:53:34 +0800

    format logging timestamp

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     stream=sys.stdout,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S' # Format time to seconds
 )
 
 @asynccontextmanager
